3f40880f420ef3874a00966e8dcb574c0c51e89d
Ronnie <digiron242@gmail.com>
2023-08-28 18:54:41 +1000

    fresh yo build

---
+++ src/webparts/spoSpfxAlertsWebpart/components/ISpoSpfxAlertsWebpartProps.ts
@@ -0,0 +1,7 @@
+export interface ISpoSpfxAlertsWebpartProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
